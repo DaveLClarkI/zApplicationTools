0a3c84d97e5b5d70d8c94c8be639a9ba2da31997
Dave L Clark I <davelclark1@gmail.com>
2021-06-09 14:06:57 -0400

    Add files via upload

--- CHKPWRJB.C
+++ CHKPWRJB.C
@@ -1,6 +1,6 @@
-       01  CHKPWRJB-PARMS.                                                      
-         03  CHK-STATUS                PIC  X(02)   VALUE SPACES.               
-         03  CHK-PARTID                PIC  X(02)   VALUE SPACES.               
-         03  CHK-PWRJOB                PIC  X(08)   VALUE SPACES.               
-         03  CHK-CURPGM                PIC  X(08)   VALUE SPACES.               
-         03  CHK-RESRVD                PIC  X(108)  VALUE LOW-VALUES.           
+       01  CHKPWRJB-PARMS.
+         03  CHK-STATUS                PIC  X(02)   VALUE SPACES.
+         03  CHK-PARTID                PIC  X(02)   VALUE SPACES.
+         03  CHK-PWRJOB                PIC  X(08)   VALUE SPACES.
+         03  CHK-CURPGM                PIC  X(08)   VALUE SPACES.
+         03  CHK-RESRVD                PIC  X(108)  VALUE LOW-VALUES.
