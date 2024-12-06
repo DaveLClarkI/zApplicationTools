2ddaf51ef5a6650cb30a863f6b67cc24958e0eb6
Dave L Clark I <davelclark1@gmail.com>
2024-12-06 12:02:57 -0500

    Add files via upload

---
+++ CHKPGMNM.C
@@ -0,0 +1,6 @@
+       01  CHKPGMNM-PARMS.
+         03  CHK-STATUS                PIC  X(02)   VALUE SPACES.
+         03  CHK-PARTID                PIC  X(02)   VALUE SPACES.
+         03  CHK-PROGNM                PIC  X(08)   VALUE SPACES.
+         03  CHK-PWRJOB                PIC  X(08)   VALUE SPACES.
+         03  CHK-RESRVD                PIC  X(108)  VALUE LOW-VALUES.
